9ba2b9a58d57a8790e9ba05bec24fd220b6e0d56
Adam Kwiasowski <kwiasekkwiasek@gmail.com>
2024-01-14 21:38:32 +0100

    Added cors

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 require("dotenv").config();
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const session = require("express-session");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcrypt");
 const User = require("./models/user");
+const cors = require("cors");
 
 const mongoose = require("mongoose");
 const db = process.env.MONGODB_URI;
@@ -19,25 +20,27 @@ async function main() {
   await mongoose.connect(db);
 }
 
-passport.use(new LocalStrategy(async (username, password, done) => {
-  try {
-    const user = await User.findOne({ username: username });
-    if (!user) {
-      return done(null, false, { message: "Incorrect username" });
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
     }
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
-      return done(null, false, { message: "Incorrect password" });
-    }
-    return done(null, user);
-  } catch (err) {
-    return done(err);
-  }
-}));
+  })
+);
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
-})
+});
 
 passport.deserializeUser(async (id, done) => {
   try {
@@ -46,47 +49,50 @@ passport.deserializeUser(async (id, done) => {
   } catch (err) {
     done(err);
   }
-})
+});
 
 const app = express();
-app.use(session({
-  secret: "super tajne haslo",
-  resave: false,
-  saveUninitialized: true
-}))
 
-const indexRouter = require('./routes/index');
+app.use(cors());
 
+app.use(
+  session({
+    secret: "super tajne haslo",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
+const indexRouter = require("./routes/index");
 
 app.use(passport.initialize());
 app.use(passport.session());
 app.set("view engine", "jade");
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   next();
-})
-app.use('/', indexRouter);
+});
+app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
